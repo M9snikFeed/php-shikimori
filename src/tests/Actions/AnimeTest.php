7d5250cb8aff7411b0c7c4bf7c4a5055aa8804a7
M9snikFeed <den4ic2001@gmail.com>
2022-02-23 15:40:37 +0300

    Updated test.

--- src/tests/Actions/AnimeTest.php
+++ src/tests/Actions/AnimeTest.php
@@ -2,10 +2,11 @@
 
 namespace M9snikfeed\PhpShikimori\Tests\Actions;
 
-use M9snikfeed\PhpShikimori\Client\ShikimoriApiClient;
 use M9snikfeed\PhpShikimori\Exceptions\ApiRequestException;
 use M9snikfeed\PhpShikimori\Models\Anime;
+use M9snikfeed\PhpShikimori\Actions\Anime as AnimeModel;
 use M9snikfeed\PhpShikimori\Models\Image;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
@@ -15,13 +16,18 @@ use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 class AnimeTest extends TestCase
 {
     /**
-     * @var ShikimoriApiClient
+     * @var \M9snikfeed\PhpShikimori\Actions\Anime
      */
-    private ShikimoriApiClient $client;
+    private AnimeModel $animeAction;
+
+    private MockObject $request;
 
     public function setUp(): void
     {
-        $this->client = new ShikimoriApiClient('php-shikimori-sdk');
+        $this->request = $this->getMockBuilder('M9snikfeed\PhpShikimori\Client\ShikimoriApiRequest')
+            ->setConstructorArgs(['https://shikimori.one/api/', 'test-api'])->getMock();
+
+        $this->animeAction = new AnimeModel($this->request);
     }
 
     /**
@@ -31,27 +37,30 @@ class AnimeTest extends TestCase
      * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
      * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
      * @throws \m9snikfeed\phpShikimori\exceptions\ApiRequestException
-     * @covers ShikimoriApiClient:anime()
+     * @covers \M9snikfeed\PhpShikimori\Actions\Anime->get()
      */
     public function testGetAnime()
     {
-        $anime = $this->client->anime()->get(1);
-        $this->assertInstanceOf(Anime::class, $anime);
-        $this->assertInstanceOf(Image::class, $anime->getImage());
+        $json = json_decode(file_get_contents(__DIR__ . '/../Data/anime_1.json'));
+        $this->request->method('get')->willReturn((array) $json);
+        $this->request->expects($this->once())->method('get');
 
-        $this->assertEquals('Cowboy Bebop', $anime->getName());
-        $this->assertEquals('Ковбой Бибоп', $anime->getRussian());
-        $this->assertEquals('/animes/1-cowboy-bebop', $anime->getUrl());
-        $this->assertEquals('tv', $anime->getKind());
-        $this->assertIsFloat($anime->getScore());
-        $this->assertEquals('released', $anime->getStatus());
-        $this->assertEquals(26, $anime->getEpisodes());
-        $this->assertEquals(0, $anime->getEpisodesAired());
-        $this->assertEquals('1998-04-03', $anime->getAiredOn());
-        $this->assertEquals('1999-04-24', $anime->getReleasedOn());
-        $this->assertEquals('r_plus', $anime->getRating());
-        $this->assertIsArray($anime->getEnglish());
-        $this->assertIsArray($anime->getJapanese());
+        $animeData = $this->animeAction->get(1);
+        $this->assertInstanceOf(Anime::class, $animeData);
+        $this->assertInstanceOf(Image::class, $animeData->getImage());
+        $this->assertEquals('Cowboy Bebop', $animeData->getName());
+        $this->assertEquals('Ковбой Бибоп', $animeData->getRussian());
+        $this->assertEquals('/animes/1-cowboy-bebop', $animeData->getUrl());
+        $this->assertEquals('tv', $animeData->getKind());
+        $this->assertIsFloat($animeData->getScore());
+        $this->assertEquals('released', $animeData->getStatus());
+        $this->assertEquals(26, $animeData->getEpisodes());
+        $this->assertEquals(0, $animeData->getEpisodesAired());
+        $this->assertEquals('1998-04-03', $animeData->getAiredOn());
+        $this->assertEquals('1999-04-24', $animeData->getReleasedOn());
+        $this->assertEquals('r_plus', $animeData->getRating());
+        $this->assertIsArray($animeData->getEnglish());
+        $this->assertIsArray($animeData->getJapanese());
     }
 
     /**
@@ -60,12 +69,13 @@ class AnimeTest extends TestCase
      * @throws RedirectionExceptionInterface
      * @throws ServerExceptionInterface
      * @throws TransportExceptionInterface
-     * @covers \M9snikfeed\PhpShikimori\Actions\Anime:get()
+     * @covers \M9snikfeed\PhpShikimori\Actions\Anime->get()
      */
     public function testAnimeNotFound()
     {
         try {
-            $anime = $this->client->anime()->get(2);
+            $this->request->method('get')->willThrowException(new ApiRequestException(code: 404));
+            $this->animeAction->get(2);
         } catch (ApiRequestException $e) {
             $this->assertSame($e->getCode(), 404);
         }
